6f72bd9bc399491bb5df187d76136bc649a08299
Cesar Herrera <cesarherr321@gmail.com>
2024-01-13 21:09:19 -0300

    config controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,6 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:user).where(user_id: params[:user_id])
+    @user = User.find_by(id: params[:user_id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
-  def index; end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    return unless @user.nil?
+
+    flash[:alert] = 'User not found'
+    redirect_to users_path
+  end
 end
